fa3e5f3c6b9d35c7b164e3f6bd4eb07fedac44bf
M.Sajid <sardarsajidjee@gmail.com>
2026-03-02 10:43:57 +0500

    Update App.js

--- App.js
+++ App.js
@@ -1,39 +1,42 @@
- document.getElementById("generateBtn").addEventListener("click", function () {
+window.onload = function () {
 
-  const numbersInput = document.getElementById("numbers").value.trim();
-  const message = document.getElementById("Message").value.trim();
-  const linksDiv = document.getElementById("links");
+  const btn = document.getElementById("generateBtn");
 
-  linksDiv.innerHTML = ""; // clear old links
+  btn.addEventListener("click", function () {
 
-  if (!numbersInput || !message) {
-    alert("Please enter numbers and message");
-    return;
-  }
+    const numbersInput = document.getElementById("numbers").value.trim();
+    const message = document.getElementById("Message").value.trim();
+    const linksDiv = document.getElementById("links");
 
-  const numbers = numbersInput.split("\n");
+    linksDiv.innerHTML = "";
 
-  numbers.forEach(function (number) {
-
-    number = number.trim();
-
-    if (number !== "") {
-
-      const cleanNumber = number.replace(/\D/g, "");
-
-      const encodedMessage = encodeURIComponent(message);
-
-      const waLink = "https://wa.me/" + cleanNumber + "?text=" + encodedMessage;
-
-      const btn = document.createElement("button");
-      btn.textContent = "Send to " + cleanNumber;
-
-      btn.onclick = function () {
-        window.open(waLink, "_blank");
-      };
-
-      linksDiv.appendChild(btn);
+    if (!numbersInput || !message) {
+      alert("Please enter numbers and message");
+      return;
     }
+
+    const numbers = numbersInput.split("\n");
+
+    numbers.forEach(function (number) {
+
+      number = number.trim();
+      if (number !== "") {
+
+        const cleanNumber = number.replace(/\D/g, "");
+        const encodedMessage = encodeURIComponent(message);
+        const waLink = "https://wa.me/" + cleanNumber + "?text=" + encodedMessage;
+
+        const button = document.createElement("button");
+        button.innerText = "Send to " + cleanNumber;
+
+        button.onclick = function () {
+          window.open(waLink, "_blank");
+        };
+
+        linksDiv.appendChild(button);
+      }
+    });
+
   });
 
-});
+};
